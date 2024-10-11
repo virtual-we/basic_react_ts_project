5a69c2c080a2002674c36782d7e039d37282814f
Aman Kumar <akdbg65@gmail.com>
2024-10-11 10:45:58 +0000

    added edit and delete feature

--- todoBasic/src/App.tsx
+++ todoBasic/src/App.tsx
@@ -4,39 +4,40 @@ import { TodoForm } from './components/todoForm/TodoForm'
 import { TodoList } from './components/todos/TodoList';
 import { TodoCategory } from './components/enums/TodoCategory';
 
-interface Todo{
+interface Todo {
   id: number,
   text: string,
   category: TodoCategory
 }
 
-const App: React.FC=()=> {
-  const [todos, setTodos]=useState<Todo[]>([]);
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodo=(text: string, category: TodoCategory)=>{
+  const addTodo = (text: string, category: TodoCategory) => {
 
-    const newTodo: Todo={
-      id:Date.now(),
+    const newTodo: Todo = {
+      id: Date.now(),
       text,
       category
     };
     setTodos([...todos, newTodo])
   }
 
-  onEditTodo(id: Number, newText: String, newCategory: TodoCategory){
-    setTodos(todos.map(todo=> todo.id===id?{...todo, Text=newText, category=newCategory}: todo))
+  const onEditTodo = (id: number, newText: string, newCategory: TodoCategory) => {
+    setTodos(todos.map(todo => 
+      todo.id === id ? { ...todo, text: newText, category: newCategory } : todo
+    ));
+  };
 
-  }
 
-
-  const onDelete(id:number){
-    setTodos(todos.filter((todo)=> todo.id!=id));
+  const onDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id != id));
   }
 
   return (
     <>
-      <TodoForm addTodo= {addTodo} />
-      <TodoList todos= {todos, onDelete={onDelete}, onEditTodo={onEditTodo}} />
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} onDelete={onDelete} onEditTodo={onEditTodo} />
     </>
   )
 }
--- todoBasic/src/components/todos/TodoList.tsx
+++ todoBasic/src/components/todos/TodoList.tsx
@@ -1,30 +1,63 @@
 import React, { useState } from 'react'
 import { TodoCategory } from '../enums/TodoCategory'
 
-interface Todo{
-    id: number,
-    text: string,
-    category: TodoCategory,
+interface Todo {
+  id: number,
+  text: string,
+  category: TodoCategory,
 }
 
-interface TodoListProp{
+interface TodoListProp {
   todos: Todo[];
-  onDelete:(id:number)=>void,
-  onEditTodo: (id: number, newText: string, newCategory: TodoCategory)=>void
+  onDelete: (id: number) => void,
+  onEditTodo: (id: number, newText: string, newCategory: TodoCategory) => void
 }
 
-export const TodoList: React.FC<TodoListProp> = ({todos, onDelete, onEditTodo}) => {
-  const [editID, setEditId]= useState<number | null> (null);
+export const TodoList: React.FC<TodoListProp> = ({ todos, onDelete, onEditTodo }) => {
+  const [editID, setEditId] = useState<number | null>(null);
   const [editText, setEditText] = useState<string>('');
   const [editCatogery, setEditCatogery] = useState<TodoCategory>(TodoCategory.Personal);
+
+  const handleEdit = (todo: Todo) => {
+    setEditId(todo.id)
+    setEditText(todo.text)
+    setEditCatogery(todo.category)
+  }
+
+  const handleSave = () => {
+    if (editID != null) {
+      onEditTodo(editID, editText, editCatogery)
+      setEditId(null)
+    }
+  }
+
   return (
-    <>
     <ul>
-    {todos.map((todo)=>
-        <li key= {todo.id}>{todo.text}-&gt; Category:-{todo.category}</li>)} 
+      {todos.map((todo) =>
+        <li key={todo.id}>
+          {editID === todo.id ? (
+            <>
+              <input value={editText} onChange={e => setEditText(e.target.value)} />
+              <select value={editCatogery}>
+                {Object.values(TodoCategory).map((cat) =>
+                  <option key={cat} value={cat}>{cat}</option>
+                )}
+              </select>
+              <button onClick={handleSave}>Save</button>
+            </>
+          ) : <>
+            
+            {todo.text}:-&gt; category: {todo.category}
+            <button onClick={() => handleEdit(todo)}>Edit</button>
+            <button onClick={() => onDelete(todo.id)}>Delete</button>
+            
+          </>
+          }
+        </li>
+
+      )}
     </ul>
 
-    </>
   )
 }
 
